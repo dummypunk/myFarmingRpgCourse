c448e312eca18bd6d0edf00ef7206207839d458f
DESKTOP-TCGK806\傻punk <401372507@qq.com>
2025-08-26 21:12:57 +0800

    三十三节half

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -1,4 +1,69 @@
-﻿public enum ToolEffect
+﻿public enum AnimationName
+{
+    idleDown,
+    idleUp,
+    idleRight,
+    idleLeft,
+    walkUp,
+    walkDown,
+    walkRight,
+    walkLeft,
+    runUp,
+    runDown,
+    runRight,
+    runLeft,
+    useToolUp,
+    useToolDown,
+    useToolRight,
+    useToolLeft,
+    swingToolUp,
+    swingToolDown,
+    swingToolRight,
+    swingToolLeft,
+    liftToolUp,
+    liftToolDown,
+    liftToolRight,
+    liftToolLeft,
+    holdToolUp,
+    holdToolDown,
+    holdToolRight,
+    holdToolLeft,
+    pickUp,
+    pickDown,
+    pickRight,
+    pickLeft,
+    count
+}
+
+public enum CharacterPartAnimator
+{
+    body,
+    arms,
+    hair,
+    tool,
+    hat,
+    count
+}
+
+public enum PartVariantColour
+{
+    none,
+    count
+}
+
+public enum PartVariantType
+{
+    none,
+    carry,
+    hoe,
+    pickaxe,
+    axe,
+    scythe,
+    wateringCan,
+    count
+}
+
+public enum ToolEffect
 {
     none,
     watering

aed3240f17f4e5ebb19bc9772c3cda28f20ba98d
Xui <401372507@qq.com>
2025-10-14 10:54:51 +0800

    57Course Core ClassFor Crops

--- Assets/Scripts/Crop/CropDetail.cs
+++ Assets/Scripts/Crop/CropDetail.cs
@@ -23,11 +23,41 @@ public class CropDetail
     
     [ItemCodeDescription]
     public int[] harvestToolItemCode; // 可用于收割的工具的物品代码数组，若无工具需求则为0
-    public int[] requireHarvestActions; // 对应收割工具所需的收割动作次数
+    public int[] requiredHarvestActions; // 对应收割工具所需的收割动作次数
     
     [ItemCodeDescription]
     public int[] cropProducedItemCode; // 收割后产出的物品代码数组
     public int[] cropProducedMinQuantity; // 产出作物的最小数量数组
     public int[] cropProducedMaxQuantity; // 产出作物的最大数量数组，若大于最小数量则随机生成最小到最大之间的数量
     public int daysToRegrow; // 下次再生的天数，若为单次作物则为-1
+    
+    /// <summary>
+    /// 如果该工具物品代码可用于收割此作物则返回 true，否则返回 false
+    /// </summary>
+    public bool CanUseToolToHarvestCrop(int toolItemCode)
+    {
+        if (RequiredHarvestActionsForTool(toolItemCode) == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 如果该工具不能用于收割此作物则返回 -1，否则返回此工具所需的收割动作次数
+    /// </summary>
+    public int RequiredHarvestActionsForTool(int toolItemCode)
+    {
+        for (int i = 0; i < harvestToolItemCode.Length; i++)
+        {
+            if (harvestToolItemCode[i] == toolItemCode)
+            {
+                return requiredHarvestActions[i];
+            }
+        }
+        return -1;
+    }
 }
---
+++ Assets/Scripts/Crop/SO_CropDetailsList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CropDetailsList", menuName = "Scriptable Objects/Crop Details List")]
+public class CropDetailsList : ScriptableObject
+{
+    [SerializeField] public List<CropDetail> cropDetails;
+
+    public CropDetail GetCropDetail(int seedItemCode)
+    {
+        return cropDetails.Find(x => x.seedItemCode == seedItemCode);
+    }
+}
